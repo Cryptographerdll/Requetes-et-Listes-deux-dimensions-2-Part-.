fbad09b59421508bc6610a400dc785c9121f7daa
Cryptographer <85350190+khalilDup@users.noreply.github.com>
2021-12-31 16:46:26 +0100

    Add files via upload

---
+++ Personne.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requêtes_et_listes_à_2_dimensions__.NET_
+{
+    class Personne
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+        private char sexe;
+        private bool est_ingénieur;
+
+        public Personne(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+
+        public Personne(string nom, string prenom, int age, char sexe, bool est_ingénieur) : this(nom, prenom)
+        {
+            this.age = age;
+            this.sexe = sexe;
+            this.est_ingénieur = est_ingénieur;
+        }
+
+        #region Getters & Setters
+
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+
+            set
+            {
+                this.nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return this.prenom;
+            }
+
+            set
+            {
+                this.prenom = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public char Sexe
+        {
+            get
+            {
+                return this.sexe;
+            }
+
+            set
+            {
+                this.sexe = value;
+            }
+        }
+
+        public bool Est_Ingénieur
+        {
+            get
+            {
+                return this.est_ingénieur;
+            }
+
+            set
+            {
+                this.est_ingénieur = value;
+            }
+        }
+
+        #endregion Getters & Setters 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Requêtes_et_listes_à_2_dimensions__.NET_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List< List<Personne> > Personnes= new List< List<Personne> >
+            {
+                new List<Personne> ()
+                {
+                    new Personne ("Jack","Well"),
+                    new Personne ("Eman","Nola"),
+                    new Personne ("Olaand","Dubov"),
+                },
+
+                new List<Personne> ()
+                {
+                    new Personne ("Ex_1","ap12"),
+                    new Personne ("kman","Taylor"),
+                    new Personne ("james","Polo"),
+                    new Personne ("Olamn","taylor"),
+                    new Personne ("Awift","mono")
+                },
+
+                new List<Personne> ()
+                {
+                    new Personne ("Samanta","oplm"),
+                    new Personne ("Lorem","Ipsum"),
+                    new Personne ("Kiven","Idial"),
+                    new Personne ("Cwift","Kono"),
+                    new Personne ("Hwift","Xono")
+                }
+            };
+
+            // Récuperer tous les Nom dont la longueur est Supérieur à 5 .
+
+            var LongQuery = from p in Personnes
+                            from person in p
+                            where person.Nom.Length > 5
+                            select person;
+
+            foreach (var Long in LongQuery)
+                WriteLine("The name is: " + Long.Nom + " Le prenom: " + Long.Prenom);
+
+            WriteLine("-------------------------------------------------------------------------");
+
+            //// Récuperer tous les Noms qui contient un "e" . 
+            //// Récuperer tous les Prenoms qui contient un "a" . 
+            //// Trie par ordre décroissant .
+            //// Créer un objet anonyme qui contient un attribut Identité = Nom + Prenom .
+
+            var Contain = from p in Personnes
+                          from person in p
+                          where person.Nom.ToLower().Contains("e")
+                          where person.Prenom.Contains("a")
+                          orderby person.Nom descending
+                          select new { Identité = person.Nom + person.Prenom };
+
+            foreach (var contain in Contain)
+            {
+                WriteLine("The Identité is: \t" + contain.Identité);
+            }
+
+            WriteLine("-------------------------------------------------------------------------");
+
+            // Récupérer toutes les listes qui contiennent plus de 4 personnes
+            // Dont le nom commence par A et B et C .
+            // Tri croissant
+            // Objet anonyme qui contient 1er lettre du nom + 1er lettre du prenom.
+
+
+            var Result = from lp in Personnes
+                         where lp.Count > 4
+                         from rec in lp
+                         where rec.Nom.StartsWith("A")
+                         where rec.Nom.StartsWith("B")
+                         where rec.Nom.StartsWith("C")
+                         orderby rec ascending
+                         select new { initiale = rec.Nom[0] + "." + rec.Prenom[0] };
+
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item.initiale);
+            }
+        }
+    }
+}
